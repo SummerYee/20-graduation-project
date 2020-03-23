0fac91b73920e613d24512f3d51960d2b382233c
王小易 <49029009+SummerYee@users.noreply.github.com>
2020-03-23 20:17:47 +0800

    update

---
+++ Design.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Design.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setEnabled(True)
+        MainWindow.resize(640, 400)
+        MainWindow.setMinimumSize(QtCore.QSize(640, 400))
+        MainWindow.setMaximumSize(QtCore.QSize(640, 400))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("小易.jpg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setMinimumSize(QtCore.QSize(0, 0))
+        self.centralwidget.setMaximumSize(QtCore.QSize(16777215, 16777215))
+        self.centralwidget.setObjectName("centralwidget")
+        self.input = QtWidgets.QTextEdit(self.centralwidget)
+        self.input.setGeometry(QtCore.QRect(11, 33, 608, 109))
+        self.input.setObjectName("input")
+        self.cancel = QtWidgets.QPushButton(self.centralwidget)
+        self.cancel.setGeometry(QtCore.QRect(330, 310, 93, 28))
+        self.cancel.setObjectName("cancel")
+        self.output = QtWidgets.QTextBrowser(self.centralwidget)
+        self.output.setGeometry(QtCore.QRect(11, 171, 608, 109))
+        self.output.setObjectName("output")
+        self.in1 = QtWidgets.QLabel(self.centralwidget)
+        self.in1.setGeometry(QtCore.QRect(11, 11, 75, 15))
+        self.in1.setObjectName("in1")
+        self.out = QtWidgets.QLabel(self.centralwidget)
+        self.out.setGeometry(QtCore.QRect(11, 149, 60, 16))
+        self.out.setObjectName("out")
+        self.run = QtWidgets.QPushButton(self.centralwidget)
+        self.run.setGeometry(QtCore.QRect(170, 310, 93, 28))
+        self.run.setObjectName("run")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "意图识别"))
+        self.cancel.setText(_translate("MainWindow", "退出"))
+        self.in1.setText(_translate("MainWindow", "请输入句子"))
+        self.out.setText(_translate("MainWindow", "显示结果"))
+        self.run.setText(_translate("MainWindow", "运行"))
---
+++ Show.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# @Author  : 王小易 / SummerYee
+# @Time    : 2020/3/23 16:34
+# @File    : 20-graduation-project Show.py
+# @Software: PyCharm
+
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from Design import Ui_MainWindow
+# import  main
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+        # 添加登录按钮信号和槽。
+        self.run.clicked.connect(self.display)
+        # 添加退出按钮信号和槽。调用close函数
+        self.cancel.clicked.connect(self.close)
+
+
+    def display(self):
+        # 利用line Edit控件对象text()函数获取界面输入
+        username = self.input.toPlainText()
+        #password = self.pwd_lineEdit.text()
+        # 利用text Browser控件对象setText()函数设置界面显示
+        self.output.setText( "用户名是: " + username)
+
+
+if __name__ == "__main__":
+    # 固定的，PyQt5程序都需要QApplication对象。sys.argv是命令行参数列表，确保程序可以双击运行
+    app = QApplication(sys.argv)
+    #初始化
+    myWin = MainWindow()
+    # 将窗口控件显示在屏幕上
+    myWin.show()
+    # 程序运行，sys.exit方法确保程序完整退出。
+    sys.exit(app.exec_())
+
